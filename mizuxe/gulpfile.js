7b959c4fd59d1a54bd0361e4c4d6361d2dabb2dc
avinash <avigupt102@gmail.com>
2018-04-16 23:19:01 +0530

    Add 3 templates

---
+++ mizuxe/gulpfile.js
@@ -0,0 +1,67 @@
+const gulp =require('gulp');
+const browserSync =require('browser-sync').create();
+const sass =require('gulp-sass');
+
+
+//compile saas and inject into browser
+
+gulp.task('sass',function(){
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());  
+})
+
+//compile js and move src/js
+
+gulp.task('js',function(){
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js',
+                     'node_modules/jquery/dist/jquery.min.js',
+                     'node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.stream())
+})
+
+
+
+
+//Watch Sass and Server
+
+gulp.task('serve', ['sass'],function(){
+    browserSync.init({
+        server:"./src"
+    });
+    
+ gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'],['sass']);
+ gulp.watch('src/*.html').on('change', browserSync.reload);
+});
+
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./src"  
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// move fontCss to src/css
+
+
+gulp.task('fonts',function(){
+    return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('src/fonts')) 
+})
+
+
+gulp.task('fa',function(){
+    return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest('src/css'))
+})
+
+
+
+
+gulp.task('default',['js','serve','fa','fonts']);
